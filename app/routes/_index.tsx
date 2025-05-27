6bc9b37ade4cd18eea192eee829fee949f8381da
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-05-27 17:47:42 +0300

    Add default child route to fill the Outlet

---
+++ app/routes/_index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+        <p id="index-page">
+            This is a demo index page for Hristiyan's Remix project.
+            <br />
+            Check out{" "}
+            <a href="https://github.com/Hristiyan-Petrov/contact-list">the GitHub repository</a>.
+        </p>
+    );
+};
